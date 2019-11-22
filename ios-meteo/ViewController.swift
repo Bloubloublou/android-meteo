269f97458749ecaa5a51e4ac5d40aa8a7c6f5965
b_lagouge <benedicte.lagouge@lucielabs.com>
2019-11-22 21:21:08 +0100

    init project

---
+++ ios-meteo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ios-meteo
+//
+//  Created by Christelle on 22/11/2019.
+//  Copyright © 2019 b_lagouge. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
